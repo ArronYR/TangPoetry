500762397303fd92699c0574ae6245d3966fd8fa
ArronYR <yangyun4814@gmail.com>
2016-04-02 15:03:30 +0800

    finish about page

--- tp_frontend/src/pages/about.jsx
+++ tp_frontend/src/pages/about.jsx
@@ -3,9 +3,19 @@
 const React = require('react');
 
 class About extends React.Component {
-    render() {
-        return <div>About</div>;
-    }
+  render() {
+    return (
+      <div className="about">
+        <h1>唐诗小站</h1>
+        <p>A small site about Tang poetry and create by react.js</p>
+        <ul className="actions vertical">
+          <li><a href="https://github.com/ArronYR/TangPoetry" className="button special fit">Github source</a></li>
+          <li><a href="http://blog.helloarron.com" className="button fit">Personal site</a></li>
+        </ul>
+        <p className="note">( If you like this project, please give a star ~~)</p>
+      </div>
+    )
+  }
 }
 
 module.exports = About;
--- tp_frontend/src/pages/main.jsx
+++ tp_frontend/src/pages/main.jsx
@@ -3,9 +3,17 @@
 const React = require('react');
 
 class Main extends React.Component {
-    render() {
-        return <div>Main</div>;
-    }
+  render() {
+    return (
+      <div className="about">
+        <h1>唐诗小站</h1>
+        <p>A small site about Tang poetry and create by react.js</p>
+        <ul className="actions vertical">
+          <li><a href="javascript:void(0);" className="button fit">Change</a></li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 module.exports = Main;
