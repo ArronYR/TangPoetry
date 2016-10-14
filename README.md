6a4d8ab142ca3bc3f8be56da9d8767f1a24089cc
ArronYR <yangyun4814@gmail.com>
2016-10-14 15:18:02 +0800

    readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A open project about TangPoetry.
 
-See [demo](http://139.129.52.197:3000/)
+See [demo](http://tpf.helloarron.com/)
 
 ![poetry](http://7xrgqs.com1.z0.glb.clouddn.com/poetry.gif)
 
---
+++ TP_Android/app/src/main/java/com/helloarron/tpandroid/WelcomeActivity.java
@@ -0,0 +1,30 @@
+package com.helloarron.tpandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+/**
+ * Created by arron on 16/4/21.
+ */
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_welcome);
+
+        // 延迟几秒，然后跳转到登录页面
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
