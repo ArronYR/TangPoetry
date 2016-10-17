8aef0f88b3b23d2fbf0ec0cb7852541a4f6a274f
ArronYR <yangyun4814@gmail.com>
2016-10-17 15:16:50 +0800

    2016-10-17

---
+++ TP_Android/app/src/main/java/com/helloarron/tpandroid/config/Config.java
@@ -0,0 +1,11 @@
+package com.helloarron.tpandroid.config;
+
+/**
+ * Created by arron on 16/10/17.
+ */
+
+public class Config {
+    public final static String HOST = "http://tps.helloarron.com";
+
+    public final static String RAND_POETRY = HOST + "/poetry/rand";
+}
